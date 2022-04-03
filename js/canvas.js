6224cb7d288fb326d1a06459725160e9e21d529e
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2022-04-03 17:58:35 +0900

    send_file...

--- js/canvas.js
+++ js/canvas.js
@@ -11,7 +11,7 @@ window.onload = function () {
   ctx = canvas.getContext("2d");
 
   var saveBtn = document.getElementsByClassName("save-btn");
-
+  // saveImg
   saveBtn[0].addEventListener("click", saveImg);
   canvas.addEventListener("mousedown", listener);
   canvas.addEventListener("mousemove", listener);
@@ -63,10 +63,32 @@ function finishDraw() {
 }
 
 function getPosition(event) {
-  //console.log("getPosition");
+  //console.log("getPosition :", event);
   var x = event.pageX - canvas.offsetLeft;
   var y = event.pageY - canvas.offsetTop;
   return { X: x, Y: y };
+}
+
+// 이미지 저장
+function fileReader() {
+  var image;
+  canvas = document.getElementById("canvas");
+  var imgBase64 = canvas.toDataURL("image/jpeg", "image/octet-stream");
+  var decodImg = atob(imgBase64.split(",")[1]);
+
+  let array = [];
+  for (let i = 0; i < decodImg.length; i++) {
+    array.push(decodImg.charCodeAt(i));
+  }
+
+  var file = new Blob([new Uint8Array(array)], { type: "image/jpeg" });
+
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = (e) => {
+    console.log("파일읽기 :", e.target.result);
+    postImgServer(e.target.result);
+  };
 }
 
 // 이미지 저장
@@ -81,6 +103,13 @@ function saveImg() {
   }
 
   var file = new Blob([new Uint8Array(array)], { type: "image/jpeg" });
+
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = (e) => {
+    console.log("파일읽기 :", e.target.result);
+  };
+
   var fileName = "canvas_img_" + new Date().getMilliseconds() + ".jpg";
   var formData = new FormData();
   console.log("파일정보 : ", file, fileName);
--- js/common.js
+++ js/common.js
@@ -5,8 +5,8 @@ function write() {
 var result;
 
 function postServer(formData) {
-  var data = new FormData();
-  data.append("file", "file");
+  //var data = new FormData();
+  //data.append("file", "file");
 
   console.log("폼데이터 확인 :", formData);
   var url = "http://localhost:8000/canvas/post";
@@ -15,6 +15,44 @@ function postServer(formData) {
   var canvasRes = fetch(url, {
     method: "POST",
     body: formData,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log("에러발생 : ", err);
+    });
+  // Promise 객체에서 값 읽어오기
+  var printRes = () => {
+    canvasRes.then((a) => {
+      console.log("aaa :", a);
+      result = a;
+    });
+  };
+
+  printRes();
+}
+
+function postImgServer(image) {
+  //var data = new FormData();
+  //data.append("file", "file");
+
+  console.log("폼데이터 확인 :", image);
+  var url = "http://localhost:8000/canvas/test";
+
+  var data = {
+    image: image,
+  };
+  var dataJsonStr = JSON.stringify(data);
+  //var dataJson = JSON.parse(dataJsonStr);
+  console.log("dataJson ::: ", dataJsonStr);
+  // 서버 연결
+  var canvasRes = fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: dataJsonStr,
   })
     .then((response) => {
       return response.json();
